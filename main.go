e4f01fc2e6ec2216d8bbfbb786d516ea87e0a3c2
Jeremy Bunting <jeremy.bunting@spinifexgroup.com>
2017-05-27 10:28:01 -0700

    formatting, getting deps figured out, linting

--- main.go
+++ main.go
@@ -1,81 +1,80 @@
 package main
 
 import (
-    "googlemaps.github.io/maps"
-    "github.com/kr/pretty"
-    "golang.org/x/net/context"
-    "log"
-    "time"
-    "strconv"
-    "github.com/joho/godotenv"
-    "os"
-    "gopkg.in/gin-gonic/gin.v1"
-    "net/http"
+	"github.com/joho/godotenv"
+	"github.com/kr/pretty"
+	"golang.org/x/net/context"
+	"googlemaps.github.io/maps"
+	"gopkg.in/gin-gonic/gin.v1"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
 )
 
 func check(err error) {
-    if err != nil {
-        log.Fatalf("fatal error: %s", err)
-    }
+	if err != nil {
+		log.Fatalf("fatal error: %s", err)
+	}
 }
 
 func setupServer() {
-    router := gin.Default()
-    router.GET("/", func(c *gin.Context) {
-        from := c.Query("from")
-        to := c.Query("to")
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		from := c.Query("from")
+		to := c.Query("to")
 
-        myResp := calcEta(from, to)
-        pretty.Println(myResp)
-        c.String(http.StatusOK, "%s", myResp)
-    })
-    router.Run(":"+os.Getenv("SERVER_PORT"))
+		myResp, err := calcEta(from, to)
+        check(err)
+
+		pretty.Println(myResp)
+		c.String(http.StatusOK, "%s", myResp)
+	})
+	router.Run(":" + os.Getenv("SERVER_PORT"))
 }
 
-func calcEta(from string, to string) string {
-    c, err := maps.NewClient(maps.WithAPIKey(os.Getenv("MAPS_KEY")))
-    check(err)
+func calcEta(f string, t string) (string, error) {
+	c, err := maps.NewClient(maps.WithAPIKey(os.Getenv("MAPS_KEY")))
+	check(err)
 
-    origin := []string{from}
-    destination := []string{to}
+	origin := []string{f}
+	destination := []string{t}
 
-    now := strconv.FormatInt(time.Now().Unix(), 10)
-    eta := &maps.DistanceMatrixRequest{
-        Origins:       origin,
-        Destinations:  destination,
-        Mode:          "driving",
-        DepartureTime: now,
-        TrafficModel:  "best_guess",
-        Units:         "imperial",
-    }
-    // get the actual ETA off the distance matrix lib
-    etaResponse := getETA(eta, c)
-    apiEta := etaResponse.Rows[0].Elements[0].DurationInTraffic.String()
+	nowish := strconv.FormatInt(time.Now().Add(parseTime(os.Getenv("DEPARTURE_OFFSET"))).Unix(), 10)
 
-    // delay the time by a touch to get out to the car
-    delayedTime := time.Now().Add(addDelay(os.Getenv("DEPARTURE_OFFSET")))
-    predictedETA, err := time.ParseDuration(apiEta)
-    check(err)
+	eta := &maps.DistanceMatrixRequest{
+		Origins:       origin,
+		Destinations:  destination,
+		Mode:          "driving",
+		DepartureTime: nowish,
+		TrafficModel:  "best_guess",
+		Units:         "imperial",
+	}
+	// get the actual ETA off the distance matrix lib
+	etaResponse := getETA(eta, c)
+	predictedETA, err := time.ParseDuration(etaResponse.Rows[0].Elements[0].DurationInTraffic.String())
+	delayedTime := time.Now().Add(predictedETA)
+	check(err)
 
-    completedEta := delayedTime.Add(predictedETA).Format(time.Kitchen)
-    return completedEta
+	completedEta := delayedTime.Format(time.Kitchen)
+	return completedEta, err
 }
 
 func main() {
-    err := godotenv.Load()
-    check(err)
-
-    setupServer()
+	err := godotenv.Load()
+	check(err)
+	setupServer()
 }
 
-func addDelay(s string) time.Duration {
-    delay, err := time.ParseDuration(s)
-    check(err)
-    return delay
+func parseTime(s string) time.Duration {
+	parsedTime, err := time.ParseDuration(s)
+	check(err)
+	return parsedTime
 }
 
-func getETA(body *maps.DistanceMatrixRequest, client *maps.Client) (*maps.DistanceMatrixResponse) {
-    resp, err := client.DistanceMatrix(context.Background(), body)
-    check(err)
-    return resp
+func getETA(body *maps.DistanceMatrixRequest, client *maps.Client) *maps.DistanceMatrixResponse {
+	resp, err := client.DistanceMatrix(context.Background(), body)
+	check(err)
+	return resp
 }
