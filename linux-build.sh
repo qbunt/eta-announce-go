e2a8e74aa62c395626f1432af32353087fb8bf2c
Jeremy Bunting <buntingj@gmail.com>
2017-05-29 14:26:20 -0700

    adding arm support for various versions of raspberry pi

--- linux-build.sh
+++ linux-build.sh
@@ -2,4 +2,6 @@
 
 export GIN_MODE=release && env GOOS=linux GOARCH=amd64 GIN_MODE=release go build -o eta-amd -v github.com/qbunt/eta-announce-go
 export GIN_MODE=release && env GOOS=linux GOARCH=arm GIN_MODE=release go build -o eta-arm -v github.com/qbunt/eta-announce-go
+env GIN_MODE=release GOOS=linux GOARCH=arm GOARM=7 go build -o eta-armv7 -v github.com/qbunt/eta-announce-go
+env GIN_MODE=release GOOS=linux GOARCH=arm GOARM=6 go build -o eta-armv6 -v github.com/qbunt/eta-announce-go
 export GIN_MODE=release && env GIN_MODE=release go build -o eta-mac -v github.com/qbunt/eta-announce-go
