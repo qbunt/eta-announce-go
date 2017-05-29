a010af6cffc81318eebf7e62a4911f21c3ccbb93
Jeremy Bunting <buntingj@gmail.com>
2017-05-29 14:10:16 -0700

    adding all builds, adding build script

--- linux-build.sh
+++ linux-build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
-env GOOS=linux GOARCH=amd64 export GIN_MODE=release go build -v github.com/qbunt/eta-announce-go -o eta-announce-go-amd
-env GOOS=linux GOARCH=arm export GIN_MODE=release go build -o eta-announce-go-arm -v github.com/qbunt/eta-announce-go
+export GIN_MODE=release && env GOOS=linux GOARCH=amd64 GIN_MODE=release go build -o eta-amd -v github.com/qbunt/eta-announce-go
+export GIN_MODE=release && env GOOS=linux GOARCH=arm GIN_MODE=release go build -o eta-arm -v github.com/qbunt/eta-announce-go
+export GIN_MODE=release && env GIN_MODE=release go build -o eta-mac -v github.com/qbunt/eta-announce-go
